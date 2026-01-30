c9744b3f6522c5cbfc1365cc293751c16bee82c9
AJor Cinta Nasi Goreng <ajoorr99@gmail.com>
2026-01-30 12:26:12 +0800

    hai lagi

--- playerr.gd
+++ playerr.gd
@@ -1,25 +1,38 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCUTY = -300.0
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+func _physics_process(delta):
+	#grapitasi
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+
+	#ompat
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCUTY
+	# biar bisa jalan
+	if direction != 0:
 		velocity.x = direction * SPEED
+		
+	# biar bisa hadap kanan kiri
+		if direction > 0:
+			sprite.flip_h = false
+		else:
+			sprite.flip_h = true
+		
+	# animasi buat jalan
+		if sprite.animation != "jalan":
+			sprite.play("jalan")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = 0
+		
+	# Animasi idel
+		if sprite.animation != "idle":
+			sprite.play("idle")
 
 	move_and_slide()
